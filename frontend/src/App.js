c65788d8b695b964b1e730a936ab27b01b98aadb
nicholaswsumner <niksum15@gmail.com>
2022-10-30 08:50:13 +0000

    Basic page component set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  return (
-    <div>Me App</div>
-  )
+  return <>
+    <Router>
+      <div className='container'>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+        </Routes>
+      </div>
+    </Router>
+  </>
 }
 
 export default App
